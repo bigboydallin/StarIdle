258b558145cbc850bbe1e9e5f2594b9dde072b9a
Dallin Dmytrk <dallindmytryk@gmail.com>
2020-11-25 17:00:28 -0700

    refactored gameScene for better readability, removed logs

--- gameScene.js
+++ gameScene.js
@@ -6,55 +6,82 @@ class GameScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('plus','assets/plus.png')
-    this.load.image('minus','assets/minus.png')
+    this.load.image('plus', 'assets/plus.png')
+    this.load.image('minus', 'assets/minus.png')
   }
 
   create() {
     gameState.delta = 0;
     gameState.speed = 300;
     gameState.bg = new StarBackground(this);
-    gameState.max = 2**(gameState.layers-1) * 10
-    for (let i = 0; i < gameState.layers; i++){
-      let maxSize = gameState.max/(2**i)
-      //add element
-      gameState.elements.push(new Element(i,maxSize));
-      //add star visualizer
-      gameState.stars.push(new Star(this,i));
-      //add display panel
-      gameState.panels.push(new ElementPanel(this,i));
-    }
-    for (let i = 0; i < gameState.layers-1; i++){
-      gameState.converters.push(new Converter(i));
-      gameState.panels.push(new ConverterPanel(this,i))
-    }
-    gameState.elements[0].count = gameState.max;
-    //add power
-    gameState.power = new Power;
-    gameState.panels.push(new PowerPanel(this));
-    //testing
+    gameState.max = 2 ** (gameState.layers - 1) * 10;
+    this.createElements();
+    this.createConverters();
+    this.createPower();
   }
 
-  update(time,delta) {
+  createElements() {
+    // creates elements and displays for them
+    for (let i = 0; i < gameState.layers; i++) {
+      let maxSize = gameState.max / (2 ** i)
+      //add element
+      gameState.elements.push(new Element(i, maxSize));
+      //add star visualizer
+      gameState.stars.push(new Star(this, i));
+      //add display panel
+      gameState.panels.push(new ElementPanel(this, i));
+    }
+    gameState.elements[0].count = gameState.max;
+  }
+
+  createConverters() {
+    //creates converters and displays
+    for (let i = 0; i < gameState.layers - 1; i++) {
+      gameState.converters.push(new Converter(i));
+      gameState.panels.push(new ConverterPanel(this, i))
+    }
+  }
+
+  createPower() {
+    // creates power and displays
+    gameState.power = new Power;
+    gameState.panels.push(new PowerPanel(this));
+  }
+
+  update(time, delta) {
     gameState.delta += delta;
     //update background
-    gameState.bg.update()
-    // update stars
-    for (let i = 0; i < gameState.layers; i++){
-      gameState.stars[i].update();
-    }
-    // update panels
-    for (let i = 0; i < gameState.panels.length; i++){
-      gameState.panels[i].update()
-    }
+    gameState.bg.update();
+    this.updateStars();
+    this.updatePanels();
     //update power
     gameState.power.setPower()
-    while (gameState.delta > gameState.speed){
+    while (gameState.delta > gameState.speed) {
       //activate converters
+      this.updateConverters();
       gameState.delta -= gameState.speed;
-      for (let i = 0; i < gameState.converters.length; i++){
-        gameState.converters[i].update()
-      }
+
+    }
+  }
+
+  updateStars(){
+    //updates star visualizers
+    for (let i = 0; i < gameState.layers; i++) {
+      gameState.stars[i].update();
+    }
+  }
+
+  updatePanels(){
+    // update display panels
+    for (let i = 0; i < gameState.panels.length; i++) {
+      gameState.panels[i].update()
+    }
+  }
+
+  updateConverters(){
+    // update converters
+    for (let i = 0; i < gameState.converters.length; i++) {
+      gameState.converters[i].update();
     }
   }
 }
